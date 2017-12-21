cb22817c18268c4888003182f2b61166209713b4
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 05:06:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
 
+require 'pry'
 def key_for_min_value(name_hash)
   new_hash = {}
   name_hash.collect do |name, value|
     # if value < 2
-      new_hash = name.select{ |name, value| value < 2 }
-binding.pry
+      new_hash = name
       # new_hash = name_hash.select { |name, value| value < 2 }
     end
 
