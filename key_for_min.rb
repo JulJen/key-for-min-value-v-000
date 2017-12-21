dd09b133a13f6dcdb1fd21510ea46837b4bc2a58
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 05:00:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   new_hash = {}
   name_hash.collect do |name, value|
     if value < 2
-      name_hash.select { |name, value| value < 2 }
+      new_hash = name_hash.select { |name, value| value < 2 }
     end
   new_hash
 end
