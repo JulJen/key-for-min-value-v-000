524cf755121cb88ae868f1114ceb58fd661d4e38
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 04:35:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.select { |name, value| name = (value < 2)}
+  name_hash.select { |name, value| name = (value < 2).first}
 end
